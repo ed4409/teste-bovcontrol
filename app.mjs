7198f3c58909654b3b0f11f0a69e1d2a4e422adc
ed4409 <115589881+ed4409@users.noreply.github.com>
2023-12-28 13:48:27 -0300

    commit boot

---
+++ app.mjs
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+
+import app from "./boot/app.mjs";
+import dotenv from "dotenv/config";
+import express from "express";
+
+app(express());
